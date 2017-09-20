bfe3a4eeea94e3bcc58d7eb05f8b224a118e928b
Paul Mitchell <paul.mitchell@bjss.com>
2017-09-20 12:13:10 +0100

    Service Finder: First draft of service finder

---
+++ app/views/service-finder.js
@@ -0,0 +1,113 @@
+const distance = require('gps-distance')
+const maps = require('@google/maps').createClient({
+  key: process.env.GOOGLE_MAPS_API_KEY,
+  Promise: Promise
+})
+
+const PLURALS = {
+  pharmacy: 'pharmacies',
+  dentist: 'dentists',
+  optician: 'opticians',
+  doctor: 'doctors'
+}
+
+function geocodeAddress (address) {
+  return maps.geocode({address, region: 'uk'}).asPromise()
+    .then(response => response.json.results[0])
+}
+
+function extractGeocodedLocationFromResult (result) {
+  const { geometry: { location } } = result
+  return location
+}
+
+function placeSearch (input) {
+  const query = {
+    location: {lat: input.lat, lng: input.long},
+    type: input.find_service_type,
+    rankby: 'distance'
+  }
+
+  return maps.placesNearby(query).asPromise()
+    .then(response => response.json.results)
+}
+
+function placeDetails (places) {
+  return Promise.all(places.map(place =>
+    maps.place({placeid: place.place_id}).asPromise()
+      .then(response => response.json.result)
+      .then(transformPlaceDetailsForTemplate)
+  ))
+}
+
+function transformPlaceDetailsForTemplate (details) {
+  const transformed = {
+    id: details.id,
+    place_id: details.place_id,
+    name: details.name,
+    address: reformatAddress(details.formatted_address),
+    phone: details.formatted_phone_number,
+    rating: details.rating,
+    geometry: details.geometry,
+    hours: computeOpeningHours(details.opening_hours)
+  }
+  return transformed
+}
+
+function reformatAddress (address) {
+  return address.replace(/, (UK|United Kingdom)$/, '')
+}
+
+function computeOpeningHours (openingHours) {
+  return openingHours
+}
+
+function computeDistanceInMiles (input, place) {
+  const KM_TO_MILES_DIVISOR = 1.609344
+  const { geometry: { location: {lat, lng} } } = place
+  return distance(input.lat, input.long, lat, lng) / KM_TO_MILES_DIVISOR
+}
+
+function computeDistances (places, input) {
+  places.forEach(place => {
+    const placeDistance = computeDistanceInMiles(input, place).toFixed(1)
+    place.distance = `${placeDistance} miles`
+  })
+  return places
+}
+
+module.exports = function (input, req) {
+  input.title = 'Service Finder Results'
+  input.error = {
+    geocodingFailed: false
+  }
+
+  return geocodeAddress(input.find_service_search)
+    .then(extractGeocodedLocationFromResult)
+    .then(location => {
+      console.log('Geocoding returned', location)
+      input.lat = location.lat
+      input.long = location.lng
+      return input
+    })
+    .then(placeSearch)
+    .then(placeDetails)
+    .then(places => computeDistances(places, input))
+    .then(results => {
+      input.results = results
+      input.resultCount = results.length
+      input.resultDescription = (
+        results.length === 1
+          ? input.find_service_type
+          : PLURALS[input.find_service_type]
+      ) + ' within 2 miles of this location'
+      return input
+    })
+    .catch(e => {
+      console.log('Geocoding error:', e)
+      // consider any error to be a postcode format error
+      // TODO: more nuanced error management
+      input.error.geocodingFailed = true
+      return input
+    })
+}
