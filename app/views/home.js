1e9754ba2f25b15901c207fccdf932035c8f1efa
Paul Mitchell <paul.mitchell@bjss.com>
2017-09-26 15:41:32 +0100

    Experimental reverse-geocoding of the user's current position

--- app/views/home.js
+++ app/views/home.js
@@ -4,5 +4,6 @@ module.exports = function (input, req) {
   input.title = false
   input.lede = 'Welcome to the new NHS website. <label for="query">Search for health problems, medicines, NHS services, healthy living tips and more.</label>'
   input.hasOmniSearch = true
+  input.googleMapsLibraryUrl = `https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAPS_API_KEY}`
   return input
 }
