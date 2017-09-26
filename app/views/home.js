59f108da6f45dd0b01cd2ed9979dc1d41603bcb4
Tom Carrington <tom@visualunderground.co.uk>
2017-09-26 13:15:01 +0100

    Adding indegestion view
     - Turned hero into component with variables
    - Added ingestion view and styling

--- app/views/home.js
+++ app/views/home.js
@@ -1,4 +1,7 @@
 module.exports = function (input) {
   input.isHomepage = true
+  input.title = false
+  input.lede = 'Welcome to the new NHS website. <label for="query">Search for health problems, medicines, NHS services, healthy living tips and more.</label>'
+  input.hasOmniSearch = true
   return input
 }
