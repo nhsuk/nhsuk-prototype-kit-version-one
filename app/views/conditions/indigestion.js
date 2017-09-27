02b154fd335494a60aaa47f0bd83c89c1a827ee5
Paul Mitchell <paul.mitchell@bjss.com>
2017-09-27 12:28:31 +0100

    Conditions: use shared service finder to locate the closest open pharmacy if the user has a saved location

--- app/views/conditions/indigestion.js
+++ app/views/conditions/indigestion.js
@@ -1,7 +1,32 @@
-module.exports = function (input) {
+const serviceFinder = require('find-local-services')
+
+module.exports = function (input, req) {
   input.isHomepage = true
   input.title = 'Indigestion'
   input.lede = 'Most people have indigestion at some point. Usually, it’s not a sign of anything more serious and you can treat indigestion yourself.'
   input.hasOmniSearch = false
+
+  // if the user has saved a location, perform a search for local pharmacies
+  // and send details of the closest open one to the template
+  // as this is an asynchronous operation, it returns a Promise
+  if (req.cookies && req.cookies.savedLocation) {
+    return serviceFinder.locateAddress(req.cookies.savedLocation)
+      .then(location => serviceFinder.findLocalServices(
+        location, 'pharmacy'
+      ))
+      .then(results => {
+        if (results && results.length) {
+          const openPharmacy = results.find(result => {
+            return result.hours && result.hours.open_now
+          })
+          input.pharmacy = openPharmacy
+        }
+        return input
+      })
+      .catch(ex => {
+        console.log('Geocoding error:', ex)
+        return input
+      })
+  }
   return input
 }
