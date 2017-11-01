b8a4285cc71908a6664cdcd9de2167c567b06fa5
Tom Carrington <tom@visualunderground.co.uk>
2017-11-01 09:45:58 +0000

    Removing comments

--- gulp/styles.js
+++ gulp/styles.js
@@ -9,20 +9,9 @@ const config = require('../config.js').gulp
 const gulp = require('gulp')
 const sass = require('gulp-sass')
 const postcss = require('gulp-postcss')
-// const sourcemaps = require('gulp-sourcemaps')
 
 gulp.task('styles', () => {
   return gulp.src(config.css.src)
-    // PostCSS Tasks before Sass compilation
-    // .pipe(
-    //     postcss([
-    //       require('stylelint'),
-    //       require('postcss-reporter')({
-    //         clearReportedMessages: true
-    //       })
-    //     ],
-    //     { syntax: require('postcss-scss') })
-    // )
 
     // Sass Compilation
     .pipe(sass({
@@ -32,7 +21,6 @@ gulp.task('styles', () => {
     // PostCSS tasks after Sass compilation
     .pipe(postcss([
       require('autoprefixer')({ browsers: ['> 5%', 'last 2 versions', 'ie > 7'] }),
-      // require('postcss-cssnext'),
       require('cssnano')({
         preset: 'default'
       })
